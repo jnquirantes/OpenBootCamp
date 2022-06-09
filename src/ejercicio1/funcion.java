65de53197a2988ce493a6b5871be59d7c9c77cb3
unknown <jnquirantes@gmail.com>
2022-06-09 20:13:50 +0200

    Primer respaldo

---
+++ src/ejercicio1/funcion.java
@@ -0,0 +1,21 @@
+package ejercicio1;
+
+
+public class funcion {
+	
+	public static void main (String [] args) {
+		
+		System.out.println(sumaNumeros(12, 17.5, 22));
+		
+		
+	}
+	
+	public static double sumaNumeros(double a, double b, double c) {
+		
+		double result = a + b + c;
+		
+		return result;
+	}
+
+
+}
