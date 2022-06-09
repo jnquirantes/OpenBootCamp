2400c3d2ecb09b6ad0c4c1cb675a6b8e5a203373
unknown <jnquirantes@gmail.com>
2022-06-09 20:45:08 +0200

    Finalizado

--- src/ejercicio2/Coche.java
+++ src/ejercicio2/Coche.java
@@ -2,15 +2,6 @@ package ejercicio2;
 
 public class Coche {
 	
-	public static void main(String [] args) {
-		
-		Coche miCoche = new Coche(4);
-		
-		miCoche.aumentarPuertas();
-		
-		System.out.println(miCoche.getPuertas());
-	}
-	
 	int puertas;
 	
 	public Coche(int puertas) {
@@ -27,5 +18,15 @@ public class Coche {
 		puertas++;
 		
 	}
+	
+	public static void main(String [] args) {
+		
+		Coche miCoche = new Coche(4);
+		miCoche.aumentarPuertas();
+		System.out.println(miCoche.getPuertas());
+	}
+	
+	
+
 
 }
