3cb8b3711e4f56d3b4cf4aa71e96ac81cc95a478
unknown <jnquirantes@gmail.com>
2022-06-10 20:09:43 +0200

    Finalizado

---
+++ src/ejercicio5/Imprimir.java
@@ -0,0 +1,22 @@
+package ejercicio5;
+
+public class Imprimir {
+
+	public static void main(String[] args) {
+	
+		int a = 5;
+		long b = 50L;
+		double c = 5.5;
+		boolean d = false;
+		String e = "Soy e";
+		
+		System.out.println("El dato entero es " + a 
+				+ "\n El dato long es " + b
+				+ "\n El dato double es " + c
+				+ "\n El dato boleano es " + d
+				+ "\n La cadena de texto es: " + e);
+		
+
+	}
+
+}
