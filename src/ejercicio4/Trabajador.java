3d6471db4c413869c8ec11e11d00c5742b3d93eb
unknown <jnquirantes@gmail.com>
2022-06-10 19:31:51 +0200

    Finalizado

--- src/ejercicio4/Trabajador.java
+++ src/ejercicio4/Trabajador.java
@@ -5,8 +5,7 @@ public class Trabajador extends Persona {
 	private double salario;
 
 	public static void main(String[] args) {
-		Trabajador t1 = new Trabajador();
-		t1.setSalario(1650.20);
+		Trabajador t1 = new Trabajador(35, "Jessi", "694445588", 1650.20);
 		
 		System.out.println("El trabajador se llama " + t1.getNombre() +
 				". Tiene " + t1.getEdad() + " años y su teléfono es " + t1.getTelefono() +
@@ -15,12 +14,12 @@ public class Trabajador extends Persona {
 	}
 	
 	public Trabajador() {
-		super(35, "Jessi", "675145965");
+		super();
 		salario = 0;
 	}
 
-	public Trabajador(double salario) {
-		super();
+	public Trabajador(int edad, String nombre, String telefono, double salario) {
+		super(edad, nombre, telefono);
 		this.salario = salario;
 	}
 	
